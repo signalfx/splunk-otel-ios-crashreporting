458eb692add9647c8807cac0379681e1c1ae41ef
Darby Vernon <dvernon@splunk.com>
2023-07-31 13:38:42 -0600

    Fix fullbuild.sh

--- fullbuild.sh
+++ fullbuild.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
-set -e
+set -ex
 swiftlint --strict
 
 # Make sure the version numbers on the podspec and CrashReporting.swift match
 echo "Checking that version numbers match"
-rumVer="$(grep SplunkRumVersionString SplunkRumCrashReporting/SplunkRumCrashReporting/CrashReporting.swift | grep -o '[0-9]*\.[0-9]*\.[0-9]*')"
-podVer="$(grep s.version SplunkOtel.podspec | grep -o '[0-9]*\.[0-9]*\.[0-9]*')"
+rumVer="$(grep CrashReportingVersionString SplunkRumCrashReporting/SplunkRumCrashReporting/CrashReporting.swift | grep -o '[0-9]*\.[0-9]*\.[0-9]*')"
+podVer="$(grep s.version SplunkOtelCrashReporting.podspec | grep -o '[0-9]*\.[0-9]*\.[0-9]*')"
 if [ $podVer != $rumVer ]; then
-    echo "Error: The version numbers in SplunkOtel.podspec and SplunkRum.swift do not match"
+    echo "Error: The version numbers in SplunkOtelCrashReporting.podspec and SplunkRum.swift do not match"
     exit 1
 fi
+
+# Check the podspec is valid
+pod lib lint SplunkOtelCrashReporting.podspec
 
 xcodebuild -project SplunkRumCrashReporting/SplunkRumCrashReporting.xcodeproj -scheme SplunkRumCrashReporting -configuration Debug build
 xcodebuild -project SplunkRumCrashReporting/SplunkRumCrashReporting.xcodeproj -scheme SplunkRumCrashReporting -configuration Debug test
