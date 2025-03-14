2dbeb932b001d4a75f4c31465436c599f2d6511f
SMickelsn <122052675+SMickelsn@users.noreply.github.com>
2025-03-14 13:54:40 -0700

    Updated build script to halt swiftbuild pending release of SPM 6.1

--- fullbuild.sh
+++ fullbuild.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -ex
-#swiftlint --strict
+swiftlint
 
 # Make sure the version numbers on the podspec and CrashReporting.swift match
 echo "Checking that version numbers match"
@@ -18,10 +18,11 @@ xcodebuild -project SplunkRumCrashReporting/SplunkRumCrashReporting.xcodeproj -s
 xcodebuild -project SplunkRumCrashReporting/SplunkRumCrashReporting.xcodeproj -scheme SplunkRumCrashReporting -configuration Debug test
 xcodebuild -project SplunkRumCrashReporting/SplunkRumCrashReporting.xcodeproj -scheme SplunkRumCrashReporting -configuration Release build
 
+# TODO Re-enable this section after the release of SPM 6.1
 # Now try to do a swift build to ensure that the package dependencies are properly in synch
-rm -rf ./.build
-SIMULATOR_SDK="$(xcode-select -p)/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk"
-SIMULATOR_TARGET="arm64-apple-ios17-simulator"
-swift build -v --sdk "$SIMULATOR_SDK" --triple "$SIMULATOR_TARGET" --scratch-path "./.build/$SIMULATOR_TARGET"
+# rm -rf ./.build
+# SIMULATOR_SDK="$(xcode-select -p)/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk"
+# SIMULATOR_TARGET="arm64-apple-ios17-simulator"
+# swift build -v --sdk "$SIMULATOR_SDK" --triple "$SIMULATOR_TARGET" --scratch-path "./.build/$SIMULATOR_TARGET"
 
 echo "========= Congratulations! ========="
